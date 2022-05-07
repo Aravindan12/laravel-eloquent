206b0b3d354827110c0d1f79d5047f3f9e258c81
Aravindan12 <you@example.com>
2022-05-07 18:42:00 +0530

    laravel livewire

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Log;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
     /**
      * Get the post's image.
      * connecting the child table with class and the common column
@@ -27,5 +31,22 @@ class Post extends Model
     public function scopestatus($query,$type)
     {
         return $query->where('status',$type);
+    }
+
+
+    public static function boot() {
+  
+        parent::boot();
+  
+        /**
+         * Write code on Method
+         *
+         * @return response()
+         */
+        static::creating(function($item) {            
+            Log::info('Creating event call: '.$item); 
+  
+            $item->status = 1;
+        });
     }
 }
